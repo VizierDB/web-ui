001ef4d6db9e39420ff1f402a043de758b620a12
Michael Brachmann <mrb24@buffalo.edu>
2020-10-05 23:46:57 -0400

    oops. accidental stage.

--- src/core/util/App.js
+++ src/core/util/App.js
@@ -24,7 +24,7 @@ import { CONTENT_CHART, CONTENT_DATASET, CONTENT_ERROR } from '../resources/Outp
 
 let href = process.env.PUBLIC_URL;
 if (process.env.NODE_ENV === 'production') {
-    //href = href + '/vizier-db';
+    href = href + '/vizier-db';
 	if (href === '') {
 		href = '/'
 	}
